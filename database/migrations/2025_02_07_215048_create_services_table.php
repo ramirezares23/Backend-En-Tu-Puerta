afcc1af4b0eb0832086dfadf587c7997e2cf053f
victoria saldivia <vsaldivia@correo.unimet.edu.ve>
2025-02-13 17:22:39 -0400

    Servicios inicializados

--- database/migrations/2025_02_07_215048_create_services_table.php
+++ database/migrations/2025_02_07_215048_create_services_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->string('service_name');
+            $table->float('service_price');
             $table->timestamps();
         });
     }
