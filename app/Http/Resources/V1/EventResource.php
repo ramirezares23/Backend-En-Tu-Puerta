430f372bfe404703a36a1efe4ad7e5d56e4c6c9b
victoria saldivia <vsaldivia@correo.unimet.edu.ve>
2025-03-01 13:40:07 -0400

    Update EventResource.php

--- app/Http/Resources/V1/EventResource.php
+++ app/Http/Resources/V1/EventResource.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Http\Resources;
+namespace App\Http\Resources\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\V1\UserResource;
+use App\Http\Resources\V1\ServiceResource;
 
 class EventResource extends JsonResource
 {
@@ -14,6 +16,77 @@ class EventResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'type' => 'event',
+            'id' => $this->id,
+            'attributes' => [
+                'provider_id' => $this->provider_id,
+                'client_id' => $this->client_id,
+                'service_id' => $this->service_id,
+                'date' => $this->date,
+                'time' => $this->time,
+                'status' => $this->status,
+                'created_at' => $this->created_at,
+            ],
+            'relationships' => [
+                'provider' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->provider_id,
+                    ],
+                    'links' => [
+                        'self' => route(
+                            'users.show',
+                            [
+                                'user' => $this->provider_id
+                            ]
+                        )
+                    ]
+                ],
+                'client' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->client_id,
+                    ],
+                    'links' => [
+                        'self' => route(
+                            'users.show',
+                            [
+                                'user' => $this->client_id
+                            ]
+                        )
+                    ]
+                ],
+                'service' => [
+                    'data' => [
+                        'type' => 'service',
+                        'id' => $this->service_id,
+                    ],
+                    'links' => [
+                        'self' => route(
+                            'services.show',
+                            [
+                                'service' => $this->service_id
+                            ]
+                        )
+                    ]
+                ]
+            ],
+            'includes' => [
+                new UserResource($this->whenLoaded('provider')),
+                new UserResource($this->whenLoaded('client')),
+                new ServiceResource($this->whenLoaded('service')),
+            ],
+            'links' => [
+                [
+                    'self' => route(
+                        'events.show',
+                        [
+                            'event' => $this->id
+                        ]
+                    )
+                ]
+            ]
+        ];
     }
 }
