da1cc1da40cae96c961ce06eeab769da770caa1c
victoria saldivia <vsaldivia@correo.unimet.edu.ve>
2025-03-19 17:49:24 -0400

    Update ServiceFactory.php

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -17,9 +17,20 @@ class ServiceFactory extends Factory
      */
     public function definition(): array
     {
+
+        $verbs = ['Corte', 'Poda', 'Limpieza', 'Diseño', 'Reparación', 'Instalación', 'Maquillaje', 'Alisado', 'Coloración'];
+        $nouns = ['Cabello', 'Árboles', 'Uñas', 'Pestañas', 'Cejas', 'Jardín', 'Oficina', 'Alfombra', 'Piel'];
+        $adjectives = ['Express', 'Profesional', 'Semipermanente', 'Intensivo', 'Completo', 'Básico', 'Premium'];
+
+        // Generar un nombre de servicio aleatorio
+        $serviceName = fake()->randomElement($verbs) . ' de ' . fake()->randomElement($nouns);
+        if (fake()->boolean(50)) { // 50% de probabilidad de agregar un adjetivo
+            $serviceName .= ' ' . fake()->randomElement($adjectives);
+        }
+
         return [
             'id_provider' => User::factory(),
-            'service_name' => 'Servicio de ' . fake()->randomElement(["Peluqueria","Manicura","Pedicura","Estilista general"]),
+            'service_name' => $serviceName,
             'service_price' => fake()->randomFloat(2, 1),
             'description' => fake()->text(),
             'images_path' => json_encode(fake()->url()),
